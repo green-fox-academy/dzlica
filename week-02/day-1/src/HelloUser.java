c39a57c312b8479695c6a617a3e97d997157446a
dzlica <dzlica@gmail.com>
2017-09-11 15:20:01 +0200

    exercise 14 HelloUser with input added

---
+++ week-02/day-1/src/HelloUser.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class HelloUser {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        String myName = myScanner.nextLine();
+        System.out.println(myName);
+    }
+}
