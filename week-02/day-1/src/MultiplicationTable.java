f778f74a8596bff3c75ca329eba6253f8533a501
dzlica <dzlica@gmail.com>
2017-09-11 16:24:50 +0200

    exercise 25 MultiplicationTable added

---
+++ week-02/day-1/src/MultiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Give me a number");
+        int myNumber = myScanner.nextInt();
+        int a = 1;
+        while(a <= 15) {
+            System.out.println(myNumber + " + " + a + " = " + a * myNumber);
+            a++;
+        }
+    }
+}
