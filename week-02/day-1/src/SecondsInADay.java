f031db09a6d48e8d6777e1f519ea18c51ebf7e5a
dzlica <dzlica@gmail.com>
2017-09-11 15:09:47 +0200

    exercise 13 secondsindays added

---
+++ week-02/day-1/src/SecondsInADay.java
@@ -0,0 +1,9 @@
+public class SecondsInADay {
+    public static void main(String[] args) {
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+        int secondsinday = ((24 - currentHours) * 3600) + ((60 - currentMinutes) * 60) + (60 - currentSeconds);
+        System.out.println(secondsinday);
+    }
+}
