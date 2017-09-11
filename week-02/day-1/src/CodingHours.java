ea11678dd32483d4a39ddf9e285c6127ef7e7018
dzlica <dzlica@gmail.com>
2017-09-11 13:48:02 +0200

    Ex 4 Codinghours added

---
+++ week-02/day-1/src/CodingHours.java
@@ -0,0 +1,7 @@
+public class CodingHours {
+    public static void main(String[] args) {
+        System.out.println( "You code " + 6 * 5 * 17 + " hours a week");
+        int myNumber = (6 * 5);
+        System.out.println( "You code " + myNumber / 0.52 + "% of a working week");
+    }
+}
