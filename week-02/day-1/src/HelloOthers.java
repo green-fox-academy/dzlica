fe349c392acd93f26b42f38d88b91e78203b20d4
dzlica <dzlica@gmail.com>
2017-09-11 13:31:28 +0200

    Second weeks first 5 exercises

---
+++ week-02/day-1/src/HelloOthers.java
@@ -0,0 +1,6 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Kriszti");
+        System.out.println("Hello Tamás");
+        System.out.println("Hello Zsombor");    }
+}
---
+++ week-02/day-1/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Lica");
+        System.out.println("40");
+        System.out.println("1.66");
+    }
+}
