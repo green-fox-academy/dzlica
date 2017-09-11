d7d4dc0b16b3d14f2cf3d8633ff63fb675f9d9bd
dzlica <dzlica@gmail.com>
2017-09-11 14:07:51 +0200

    exercise 10 basicinfo added

---
+++ week-02/day-1/src/DefineBasicInfo.java
@@ -0,0 +1,12 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String myName = "Lica";
+        int myAge = 40;
+        double myHeight = 1.66;
+        boolean isMarried = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(isMarried);
+    }
+}
