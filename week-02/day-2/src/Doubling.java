c772d427d3d04d6224c12501066d264dae23df4b
dzlica <dzlica@gmail.com>
2017-09-12 13:36:51 +0200

    The one with Doubling

---
+++ week-02/day-2/src/Doubling.java
@@ -0,0 +1,14 @@
+public class Doubling {
+    public static void main(String[] args) {
+// - Create an integer variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Print the result of `doubling(ak)`
+        int ak = 123;
+        System.out.println(Doubling(ak));
+
+    }
+    public static int Doubling(int input) {
+        return input * 2;
+    }
+
+}
