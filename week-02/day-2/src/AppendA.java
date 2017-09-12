545a842814f328d777dbba66bab8e4ca55637e4f
dzlica <dzlica@gmail.com>
2017-09-12 14:14:17 +0200

    The one with the AppendA

---
+++ week-02/day-2/src/AppendA.java
@@ -0,0 +1,8 @@
+public class AppendA {
+    public static void main(String[] args) {
+        appendA("kuty");
+    }
+    public static void appendA(String am) {
+        System.out.println(am + "a");
+    }
+}
