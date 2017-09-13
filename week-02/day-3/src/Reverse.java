32cf48767df002220cc65488d1ccfdacb5b1a7ae
dzlica <dzlica@gmail.com>
2017-09-13 14:01:42 +0200

    Finished with StringBuilder, but stucked with function, to be continued

---
+++ week-02/day-3/src/Reverse.java
@@ -0,0 +1,26 @@
+public class Reverse {
+    public static void main(String... args){
+
+        String reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+        
+        // reversed = new StringBuffer(reversed).reverse().toString();
+
+
+        // Create a function that can reverse a String, which is passed as the parameter
+        // Use it on this reversed string to check it!
+        // Try to solve this using charAt() first, and optionally anything else after.
+
+        System.out.println(reversed);
+
+    }
+    public static String reversedText(String toBeReversed) {
+        for (int i = toBeReversed.length()-1; i >= 0; i--) {
+            toBeReversed = toBeReversed.charAt(i);
+            String theResult = toBeReversed
+        }
+        return toBeReversed;
+    }
+}
+// String result="";
+//for (int i=string.length()-1; i>=0; i--) {
+//result = result + string.charAt(i);
