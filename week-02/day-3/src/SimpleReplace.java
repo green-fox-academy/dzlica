99fd54d29478b3103245e4e5978c5e449bc24b94
dzlica <dzlica@gmail.com>
2017-09-13 11:11:37 +0200

    01 - The one with The Galaxy far far away, word replacement

---
+++ week-02/day-3/src/SimpleReplace.java
@@ -0,0 +1,9 @@
+public class SimpleReplace {
+    public static void main(String... args){
+        String Example = "In a dishwasher far far away";
+        // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+        // Please fix it for me!
+        // Expected ouput: In a galaxy far far away
+        System.out.println(Example.replace("dishwasher", "galaxy"));
+    }
+}
