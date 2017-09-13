92c43ef238a493946add16eac326adec6288aeac
dzlica <dzlica@gmail.com>
2017-09-13 15:17:57 +0200

    List-06 - The one with the IsInList, list contains all ellements of other list

--- week-02/day-3/src/IsInList.java
+++ week-02/day-3/src/IsInList.java
@@ -2,7 +2,14 @@ import java.util.*;
 
 public class IsInList {
     public static void main(String... args){
-        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
+        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(2, 4, 6, 8, 10, 12, 14));
+        ArrayList<Integer> smallerList = new ArrayList<Integer>(Arrays.asList(4,8,12,16));
+        if (list.containsAll(smallerList)) {
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
 
         // Check if list contains all of the following elements: 4,8,12,16
         // Print "true" if it contains all, otherwise print "false"
