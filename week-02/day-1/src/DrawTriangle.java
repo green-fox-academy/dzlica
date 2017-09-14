964a15ad154519fc2b9361b9dc841829578ec6ba
dzlica <dzlica@gmail.com>
2017-09-14 13:30:13 +0200

    Draw Triangle finished and working

--- week-02/day-1/src/DrawTriangle.java
+++ week-02/day-1/src/DrawTriangle.java
@@ -1,11 +1,25 @@
+// Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as the number was
+
 import java.util.Scanner;
 
 public class DrawTriangle {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        int a =myScanner.nextInt();
-        for (int i = 0; i<=a; i++) {
-            System.out.println("*");
+        System.out.println("Give me a number:");
+        int a = myScanner.nextInt();
+        for (int i = 0; i <= a; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
