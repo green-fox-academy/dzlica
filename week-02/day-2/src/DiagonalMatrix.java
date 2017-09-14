1f5a758b3f1f9430d868dd14b301900e9fc1c864
dzlica <dzlica@gmail.com>
2017-09-14 10:53:57 +0200

    The one with the Matrix

--- week-02/day-2/src/DiagonalMatrix.java
+++ week-02/day-2/src/DiagonalMatrix.java
@@ -10,11 +10,17 @@
 
 public class DiagonalMatrix {
     public static void main(String[] args) {
-        int [][] matrix = new int[4][4];
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                matrix[i][j] = i + j;
+        int[][] matrix = new int[4][4];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j) {
+                    matrix[i][j] = 1;
+                } else {
+                    matrix[i][j] = 0;
+                }
+
             }
+
         }
         for (int[] a : matrix) {
             for (int i : a) {
