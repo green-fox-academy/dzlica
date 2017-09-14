4efb98a3e64e1241f101c8393943f24e572ab1dc
dzlica <dzlica@gmail.com>
2017-09-14 20:32:58 +0200

    The one with the ConditionalVariableMutation, finished

--- week-02/day-1/src/ConditionalVariableMutation.java
+++ week-02/day-1/src/ConditionalVariableMutation.java
@@ -17,6 +17,13 @@ public class ConditionalVariableMutation {
         // if b is between 10 and 20 set out2 to "Sweet!"
         // if less than 10 set out2 to "More!",
         // if more than 20 set out2 to "Less!"
+        if (10 <= b && b <= 20) {
+            out2 = "Sweet";
+        }
+        else if (b < 13) {
+            out2 = "More!";
+        }
+        else out2 = "Less!";
 
 
         System.out.println(out2);
@@ -30,13 +37,21 @@ public class ConditionalVariableMutation {
         // if credits are smaller than 50,
         // and isBonus is false decrement c by 1
         // if isBonus is true c should remain the same
-
+        if (credits >= 50 && isBonus == false) {
+            c = (c - 2);
+        }
+        else if (credits < 50 && isBonus == false) {
+            c = (c - 1);
+        }
+        else if (isBonus == true) {
+            c = c;
+        }
 
         System.out.println(c);
 
 
         int d = 8;
-        int time = 120;
+        int time = 210;
         String out3 = "";
         // if d is dividable by 4
         // and time is not more than 200
@@ -44,7 +59,13 @@ public class ConditionalVariableMutation {
         // if time is more than 200
         // set out3 to "Time out"
         // otherwise set out3 to "Run Forest Run!"
-
+        if (d % 4 == 0 && time <= 200) {
+            out3 = "check";
+        }
+        else if (time > 200) {
+            out3="Time out";
+        }
+        else out3 = "Run Forest Run";
 
         System.out.println(out3);
     }
