c571681d3714a2649e6a864804d00a0afdb8f161
dzlica <dzlica@gmail.com>
2017-09-19 16:04:43 +0200

    Doubled, added for loop in for loop, still not ready

--- week-03/day-2/src/Doubled.java
+++ week-03/day-2/src/Doubled.java
@@ -14,19 +14,22 @@ public class Doubled {
         Path removedChars = Paths.get("removedChars.txt");
         try {
             List<String> deCrypt = Files.readAllLines(duplicated);
-            List<String> removed = WantToRemove(deCrypt);
+            List<String> removed = wantToRemove(deCrypt);
+            System.out.println(removed);
             Files.write(removedChars, removed);
 
         } catch (Exception e) {
             System.out.println("Something went wrong");
         }
     }
-    public static ArrayList WantToRemove(List<String> LetsDoIt) {
-        ArrayList<String > toReturn = new ArrayList<>();
-        for (int i = 0; i < LetsDoIt.size(); i++) {
-            if (LetsDoIt.size() % 2 == 0) {
-                LetsDoIt.remove(i);
+    public static ArrayList wantToRemove(List<String> letsDoIt) {
+        ArrayList<String > toReturn = new ArrayList<>(letsDoIt);
+        for (int i = 0; i < letsDoIt.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (letsDoIt.size() % 2 == 0) {
+                    toReturn.add(letsDoIt.remove(i));
                 }
+            }
             }
         return toReturn;
 
