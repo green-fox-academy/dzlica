7b4ea709b0a6c7bd661dc34008e0cff69c0e25d3
dzlica <dzlica@gmail.com>
2017-09-20 13:17:41 +0200

    Purple steps in 3d

---
+++ week-03/day-3/src/PurpleSteps3d.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps3d {
+
+    public static void mainDraw(Graphics graphics){
+        // reproduce this:
+        // [https://github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
+        int a = 10;
+        int tarolo = a;
+        graphics.setColor(Color.MAGENTA);
+        for (int i = 0; i <= 6; i++) {
+            tarolo += i*a;
+            graphics.fillRect(tarolo - i*a, tarolo - i*a, i*a, i*a);
+        }
+
+
+
+
+
+    }
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
