8e94d9765fe9b7111b298dcb54beb468f4ab8995
dzlica <dzlica@gmail.com>
2017-09-26 14:19:51 +0200

    Fixed some typos

--- week-04/day-2/src/Mentor.java
+++ week-04/day-2/src/Mentor.java
@@ -15,7 +15,7 @@ public class Mentor extends Person {
 
     @Override
     public void introduce() {
-        System.out.println("Hi, I1m " + name + ", a " + age + "year old " + gender + level + " mentor");
+        System.out.println("Hi, I'm " + name + ", a " + age + "year old " + gender + " " + level + " mentor");
     }
 }
 
