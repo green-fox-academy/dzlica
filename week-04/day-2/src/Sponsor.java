af7c6e5bbc85f086b3ab7fcca5da6074ddda9ca5
dzlica <dzlica@gmail.com>
2017-09-26 14:03:14 +0200

    Class Sponsor created

---
+++ week-04/day-2/src/Sponsor.java
@@ -0,0 +1,27 @@
+public class Sponsor extends Person{
+    String company;
+    int hiredStudents;
+
+    public Sponsor(String name, int age, String gender, String company) {
+        super(name, age, gender);
+        this.hiredStudents = 0;
+    }
+    public Sponsor() {
+        this.company = "Google";
+        this.hiredStudents = 0;
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " who represents " + company + " and hired " + hiredStudents + " students so far.");
+    }
+
+    @Override
+    public void getGoal() {
+        System.out.println("Hire brilliant junior software developers.");
+    }
+
+    public void hire() {
+        hiredStudents += 1;
+    }
+}
