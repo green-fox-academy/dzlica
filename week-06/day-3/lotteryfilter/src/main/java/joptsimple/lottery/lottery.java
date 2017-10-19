6ca24cc1515f00056a4fe7f333363da5d559e619
Magdalena Dzindzisz <dzlica@gmail.com>
2017-10-19 12:19:58 +0200

    f working, o almost there

--- week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/lottery.java
+++ week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/lottery.java
@@ -20,24 +20,35 @@ public class lottery {
         OptionParser parser = new OptionParser();
         parser.accepts("y").withRequiredArg();
         parser.accepts("f").withRequiredArg();
-        parser.accepts("o");
+        parser.accepts("o").withRequiredArg();
 
         OptionSet options = parser.parse(input);
         String year = String.valueOf(options.valueOf("y"));
-        String input1 = String.valueOf(options.valueOf("f"));
+        String inputFrom = String.valueOf(options.valueOf("f"));
+        String outputFrom = String.valueOf(options.valueOf("o"));
 
 
         if (options.has("y")) {
             try {
+                if (options.has("f")) {
+                    CSVReader reader = new CSVReader(new FileReader("/Users/lica/GreenFox/dzlica/week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/" + inputFrom));
+                }
                 CSVReader reader = new CSVReader(new FileReader("/Users/lica/GreenFox/dzlica/week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/otos.csv"));
-                CSVWriter writer = new CSVWriter(new FileWriter("/Users/lica/GreenFox/dzlica/week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/output.csv"));
                 List<String[]> lines = reader.readAll();
                 List<String[]> temp = new ArrayList();
+                if (options.has("o")) {
+                    CSVWriter writer = new CSVWriter(new FileWriter("/Users/lica/GreenFox/dzlica/week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/" + outputFrom));
+                    writer.writeAll(temp);
+                    writer.close();
+                }
+                CSVWriter writer = new CSVWriter(new FileWriter("/Users/lica/GreenFox/dzlica/week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/output.csv"));
+
                 for (int i = 0; i < lines.size(); i++) {
                     if (lines.get(i)[0].startsWith(year)) {
                         temp.add(lines.get(i));
                     }
                 }
+
                 writer.writeAll(temp);
                 writer.close();
 
@@ -46,17 +57,17 @@ public class lottery {
             }
         }
 
-        if (options.has("f")) {
-            try {
-                CSVReader reader = new CSVReader(new FileReader("/Users/lica/GreenFox/dzlica/week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/" + input1));
-                CSVWriter writer = new CSVWriter(new FileWriter("/Users/lica/GreenFox/dzlica/week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/output.csv"));
-                List<String[]> flines = reader.readAll();
-                writer.writeAll(flines);
-                writer.close();
-            } catch (Exception e) {
-                System.out.println("cannot read");
-            }
-        }
+//        if (options.has("f")) {
+//            try {
+//                CSVReader reader = new CSVReader(new FileReader("/Users/lica/GreenFox/dzlica/week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/" + inputFrom));
+//                CSVWriter writer = new CSVWriter(new FileWriter("/Users/lica/GreenFox/dzlica/week-06/day-3/lotteryfilter/src/main/java/joptsimple/lottery/output.csv"));
+//                List<String[]> flines = reader.readAll();
+//                writer.writeAll(flines);
+//                writer.close();
+//            } catch (Exception e) {
+//                System.out.println("cannot read");
+//            }
+//        }
 
     }
 }
