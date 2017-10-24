85ff8a10558833b68a00b053ce199faf78898e1f
Magdalena Dzindzisz <dzlica@gmail.com>
2017-10-24 12:15:50 +0200

    Hello woth spring

---
+++ week-07/day-1/spring-intro/src/main/java/com/greenfox/dzlica/materialreview/MaterialReviewApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.dzlica.materialreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialReviewApplication.class, args);
+	}
+}
---
+++ week-07/day-1/spring-intro/src/test/java/com/greenfox/dzlica/materialreview/MaterialReviewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfox.dzlica.materialreview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MaterialReviewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
