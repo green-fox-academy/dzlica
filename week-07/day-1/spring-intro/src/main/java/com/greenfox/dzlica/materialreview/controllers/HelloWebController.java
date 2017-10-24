7f8c5d4529eb3537b21203816b919af915440b68
Magdalena Dzindzisz <dzlica@gmail.com>
2017-10-24 15:35:58 +0200

    Greets randomly in different languages

--- week-07/day-1/spring-intro/src/main/java/com/greenfox/dzlica/materialreview/controllers/HelloWebController.java
+++ week-07/day-1/spring-intro/src/main/java/com/greenfox/dzlica/materialreview/controllers/HelloWebController.java
@@ -7,16 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
     public AtomicLong loadCounter = new AtomicLong(1);
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour", "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit", "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej", "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+    //int idx = new Random().nextInt(hellos.length);
+    //String randomHellos = (hellos[idx]);
 
     @RequestMapping("/web/greeting")
     public String greeting(Model model, @RequestParam("name") String name) {
         model.addAttribute("name", name);
         model.addAttribute("counter", loadCounter.getAndIncrement());
+        int idx = new Random().nextInt(hellos.length);
+        model.addAttribute("hello", (hellos[idx]));
         return "greeting";
     }
 
