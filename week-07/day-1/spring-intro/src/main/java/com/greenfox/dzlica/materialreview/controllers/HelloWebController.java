9fa5988700444d4c02da336f8765915721512c8e
Magdalena Dzindzisz <dzlica@gmail.com>
2017-10-24 15:22:03 +0200

    web greet counter app

--- week-07/day-1/spring-intro/src/main/java/com/greenfox/dzlica/materialreview/controllers/HelloWebController.java
+++ week-07/day-1/spring-intro/src/main/java/com/greenfox/dzlica/materialreview/controllers/HelloWebController.java
@@ -1,15 +1,24 @@
 package com.greenfox.dzlica.materialreview.controllers;
 
+import com.greenfox.dzlica.materialreview.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+    public AtomicLong loadCounter = new AtomicLong(1);
+
     @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World");
+    public String greeting(Model model, @RequestParam("name") String name) {
+        model.addAttribute("name", name);
+        model.addAttribute("counter", loadCounter.getAndIncrement());
         return "greeting";
     }
+
+
 }
