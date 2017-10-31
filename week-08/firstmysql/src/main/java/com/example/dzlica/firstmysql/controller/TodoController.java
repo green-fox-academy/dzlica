254737860b35732ba801383283208e13a8a32969
Magdalena Dzindzisz <dzlica@gmail.com>
2017-10-31 20:47:07 +0100

    Creating todos MySQL, halfway

--- week-08/firstmysql/src/main/java/com/example/dzlica/firstmysql/controller/TodoController.java
+++ week-08/firstmysql/src/main/java/com/example/dzlica/firstmysql/controller/TodoController.java
@@ -1,10 +1,11 @@
 package com.example.dzlica.firstmysql.controller;
 
+import com.example.dzlica.firstmysql.model.Todo;
 import com.example.dzlica.firstmysql.repositories.TodoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/todo")
@@ -18,4 +19,18 @@ public class TodoController {
         model.addAttribute("todos", todoRepo.findAll());
         return "todo";
     }
+
+    @GetMapping(value="/add")
+    public String addTodo(@RequestParam String title, @RequestParam boolean isUrgent, @RequestParam boolean isDone) {
+        todoRepo.save(new Todo(title, isUrgent, isDone));
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String addTodo(@ModelAttribute TodoRepo todoRepo, Model model) {
+        model.addAttribute("added", todoRepo.findAll());
+        return "todo";
+    }
+
+
 }
