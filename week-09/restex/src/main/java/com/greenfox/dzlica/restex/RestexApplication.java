b20c91196201b34d8375accb0107a54bd884b8fd
Magdalena Dzindzisz <dzlica@gmail.com>
2017-11-07 11:02:20 +0100

    project, dependencies, frontend.js added

---
+++ week-09/restex/src/main/java/com/greenfox/dzlica/restex/RestexApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.dzlica.restex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestexApplication.class, args);
+	}
+}
